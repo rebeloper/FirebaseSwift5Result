4fdf2ef8b2b3332be1ef8844030907c7e943157e
Alex <therebeloper@gmail.com>
2019-04-10 10:53:55 +0300

    Initial Commit

---
+++ FirebaseSwift5Result/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  FirebaseSwift5Result
+//
+//  Created by Alex Nagy on 10/04/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
